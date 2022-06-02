3c563afb9786680af97c476789b127adcba589fe
Pierre Beaufils <pierre.beaufils@outlook.fr>
2022-06-02 16:26:17 +0200

    fetch all players data

--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -4,26 +4,45 @@ import { Link } from "react-router-dom"
 function Players() {
   const [players, setPlayers] = useState<Array<any> | null>(null)
 
-  /* useEffect(async() => {
-      const result = await fetch(
-        'https://api.mpg.football/api/data/championship-players-pool/1',
-      );
-  
-      setPlayers(result.data)
-      fetchData()
-  }, []) */
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const response = await fetch(
+        "https://api.mpg.football/api/data/championship-players-pool/1"
+      )
+        .then((response) => response.json())
+        .catch((error) => console.error("error fetching data: ", error))
+
+      setPlayers(response.poolPlayers)
+      console.log(response)
+    }
+
+    fetchPlayers()
+  }, [])
 
   return (
     <div className="home">
-      <div className="container">
+      <div className="max-w-3xl mx-auto mt-20">
         {players && (
-          <div className="flex flex-col">
+          <table>
+            <tr>
+              <th>Joueur</th>
+              <th>Cote</th>
+              <th>Note</th>
+              <th>Buts</th>
+              <th>Poste</th>
+            </tr>
             {players.map((player) => (
-              <div className="col-lg-5" key={player.id}>
-                <p>{player.firstName}</p>
-              </div>
+              <tr className="col-lg-5" key={player.id}>
+                <td>
+                  {player.firstName} {player.lastName}
+                </td>
+                <td>{player.quotation}</td>
+                <td>{player.stats.averageRating}</td>
+                <td>{player.stats.totalGoals}</td>
+                <td>{player.ultraPosition}</td>
+              </tr>
             ))}
-          </div>
+          </table>
         )}
       </div>
     </div>
